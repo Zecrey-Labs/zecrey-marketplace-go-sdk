103e47e9307d2694b14422a2727d2f1faf46f44b
yiming <isyiming.zhang@icloud.com>
2022-11-22 16:39:08 +0800

    add

--- sdk/client.go
+++ sdk/client.go
@@ -212,6 +212,7 @@ func (c *Client) MintNft(nftInfo Mintnft) (*RespCreateAsset, error) {
 		fmt.Sprintf("/api/v1/sdk/getSdkMintNftTxInfo?account_name=%s&collection_id=%d&name=%s&content_hash=%streasury_rate%d",
 			c.AccountName, nftInfo.CollectionId, nftInfo.Name, ContentHash, nftInfo.TreasuryRate))
 	if err != nil {
+		fmt.Println("getSdkMintNftTxInfo err:", err)
 		return nil, err
 	}
 	body, err := ioutil.ReadAll(respSdkTx.Body)
